63fd3f978d82ed7647a22b74ff2db8ebd1e84401
sarapapa-sp <shubhampawar1705@gmail.com>
2022-10-14 13:04:44 +0530

    added the actions for login and register

---
+++ Actions/login.php
@@ -0,0 +1,12 @@
+<?php
+
+$username= $_POST['username'];
+$password = $_POST['password'];
+
+echo "Username -> $username";
+echo "<br>";
+echo "<br>";
+echo "Password -> $password";
+echo "<br>";echo "<br>";
+
+?>
---
+++ Actions/register.php
@@ -0,0 +1,15 @@
+<?php
+
+    $username= $_POST['username'];
+    $password = $_POST['password'];
+    $email = $_POST['useremail'];
+
+
+echo "Username -> $username";
+echo "<br>";
+echo "<br>";
+echo "Password -> $password";
+echo "<br>";echo "<br>";
+echo "EMail -> $email";
+
+?>
