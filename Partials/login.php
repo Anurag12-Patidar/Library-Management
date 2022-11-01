4551d0fcd9bdd9a0b2804ab96b09b76d127925bb
sarapapa-sp <shubhampawar1705@gmail.com>
2022-11-01 19:24:18 +0530

    added the login design

--- Partials/login.php
+++ Partials/login.php
@@ -6,25 +6,72 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../Assets/styling/login.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
 </head>
 <body>
-    <h1>Login </h1>
-    <form action="../Actions/login.php" method="post" >
-
-        <input type="text" name="username" placeholder="Enter the username">
-        <br>
-        <br>
-        <input type="password" name="password" placeholder="Enter the password">
-        <br>
-        <br>
-        <button>Login</button>
-    </form>
-    <br>
-    <br>
-    <a href="./register.php">Regsiter Page</a>
-    <br>
-    <br>
-    <br>
-    <a href="../">Home Page</a>
+    <div class="overlay">
+<!-- LOGN IN FORM by Omar Dsoky -->
+<form action="../Actions/login.php" method="post">
+   <!--   con = Container  for items in the form-->
+   <div class="con">
+   <!--     Start  header Content  -->
+   <header class="head-form">
+      <h2>Log In</h2>
+      <!--     A welcome message or an explanation of the login form -->
+      <p>login here using your username and password</p>
+   </header>
+   <!--     End  header Content  -->
+   <br>
+   <div class="field-set">
+     
+      <!--   user name -->
+         <!-- <span class="input-item">
+           <i class="fa fa-user-circle"></i>
+         </span> -->
+        <!--   user name Input-->
+         <input class="form-input" id="txt-input" type="text" placeholder="@UserName" required>
+     
+      <br>
+     
+           <!--   Password -->
+     
+      <!-- <span class="input-item">
+        <i class="fa fa-key"></i>
+       </span> -->
+      <!--   Password Input-->
+      <input class="form-input" type="password" placeholder="Password" id="pwd"  name="password" required>
+     
+<!--      Show/hide password  -->
+     <!-- <span>
+        <i class="fa fa-eye" aria-hidden="true"  type="button" id="eye"></i>
+     </span> -->
+     
+     
+      <br>
+<!--        buttons -->
+<!--      button LogIn -->
+      <button class="log-in"> Log In </button>
+   </div>
+  
+<!--   other buttons -->
+   <div class="other">
+<!--      Forgot Password button-->
+      <button class="btn submits frgt-pass">Forgot Password</button>
+<!--     Sign Up button -->
+      <button class="btn submits sign-up"> <a href="./register.php"> Sign Up </a>
+<!--         Sign Up font icon -->
+      <!-- <i class="fa fa-user-plus" aria-hidden="true"></i> -->
+      </button>
+<!--      End Other the Division -->
+   </div>
+     
+<!--   End Conrainer  -->
+  </div>
+  
+  <!-- End Form -->
+</form>
+</div>
+<script src="../Assets/js/login.js"></script>
 </body>
 </html>
